626de57bfcb88554a295e06f5deade1ee0dcfef5
IqraKhalid123 <iqrakd@gmail.com>
2022-05-11 19:20:56 +0500

    Initialize commit

---
+++ script.js
@@ -0,0 +1,43 @@
+                       //question # 1
+
+//var a = 10;
+//++a;
+//document.write("The value of a is " + a);
+
+//a++;
+//document.write('</br>' + "The value of a is " + a);
+//--a;
+//document.write('</br>' + "The value of a is " + a);
+//a--;
+//document.write('</br>' + "The value of a is " + a);
+
+    // Question # 2
+//var a = 2, b = 1;
+//--a;
+//document.write('</br>' + "The value of a is " + a);
+//var c = (--a) - (--b);
+//document.write('</br>' + "The value of a is " + c);
+//var d = (--a) - (--b) + (++b);
+//document.write('</br>' + "The value of a is " + d);
+//var result = (--a) - (--b) + (++b) + (b--);
+//document.write('</br>' + "The value of a is " + result);
+
+   //Question # 3
+//var name = prompt("what is your name?");
+//document.write("Hello!" + name);
+
+   //Question # 4
+//var num = parseInt(prompt("Enter number for multiplication table.","5"));
+//for (i = 1; i < 11; i++) {
+  //  document.write( '</br>' + num + " x " + i + " = " + num * i );
+  //}
+
+  //Question # 5
+var subject1 = prompt("write 1st subject name."); 
+var subject2 = prompt("write 2nd subject name.");  
+var subject3 = prompt("write 3rd subject name."); 
+
+var total = 100;
+var marksSubject1 = prompt("write obtained marks for 1st subject");
+var marksSubject2 = prompt("write obtained marks for 2nd subject");
+var marksSubject3 = prompt("write obtained marks for 3rd subject");
